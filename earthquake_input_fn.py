6c05ab1a975da18b0741989d8427a46579e59881
mattproetsch <mattproetsch@gmail.com>
2019-01-21 21:13:53 +0000

    added dropout, multicell RNN

--- earthquake_input_fn.py
+++ earthquake_input_fn.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import os.path as osp
 from glob import glob
 import os
+import numpy as np
 
 def _deserialize_earthquakes(serialized_examples):
     features = {
@@ -30,6 +31,20 @@ def _deserialize_earthquakes2(serialized_examples):
     features['tminus'] = tf.strings.to_number(string_tensor=features['tminus'], out_type=tf.float64)
     
     return tf.data.Dataset.from_tensor_slices(({'acousticdata': features['acousticdata']}, features['tminus']))
+
+def _deserialize_earthquakes2_predict(serialized_examples):
+    features = {
+        'acousticdata': tf.FixedLenFeature((4096), tf.int64),
+        'tminus': tf.FixedLenFeature((4096), tf.string)
+    }
+    features = tf.io.parse_single_example(
+        serialized_examples,
+        features=features
+    )
+    
+    features['tminus'] = tf.strings.to_number(string_tensor=features['tminus'], out_type=tf.float64)
+    
+    return tf.data.Dataset.from_tensor_slices({'acousticdata': features['acousticdata'], 'tminus': features['tminus']})
 
 
 def earthquake_input_fn(basedir, batch_size, timesteps, traintest='none', eager=False):
@@ -88,6 +103,89 @@ def earthquake_input_fn2(basedir, batch_size, timesteps, scales=None, traintest=
                                                                      cycle_length=batch_size,
                                                                      block_length=timesteps))
     
+    dataset = dataset.batch(timesteps).batch(batch_size)
+    dataset = dataset.prefetch(N_sub_batches * 2)
+    
+    if eager:
+        return dataset
+    
+    data_iter = dataset.make_one_shot_iterator()
+    return data_iter.get_next()
+
+def earthquake_input_fn2_prediction(basedir, batch_size, timesteps, num_files=None, scales=None, traintest='none', eager=False):
+    """
+    `scales` argument should be a list of '1eX', where X can range from -8 to 2
+    """
+    
+    assert traintest in ['train', 'test'], 'must specify traintest as "train" or "test"'
+    assert 4096 % timesteps == 0, 'timesteps must evenly divide 4096'
+    assert type(num_files) is int, 'num_files must be an int'
+    possible_scales = ['1e%d' % d for d in range(-9, 4+1)]
+    
+    files = []
+    if type(scales) is str:
+        scales = [scales]
+    for x in scales:
+        assert x in possible_scales, 'invalid scale %s, try ["1e0, 1e-2"], "1e1", etc.' % x
+        files.extend(glob(osp.join(basedir, traintest, x, '*.tfrecords')))
+    
+    files = np.random.permutation(files)[:num_files]
+    dataset = tf.data.Dataset.from_tensor_slices(files)
+    dataset = tf.data.TFRecordDataset(dataset, num_parallel_reads=24)
+    
+    # Turn each input example into a series of 4096/(timesteps) sub-examples
+    N_sub_batches = int(4096 / timesteps)    
+    dataset = dataset.apply(tf.data.experimental.parallel_interleave(lambda x: _deserialize_earthquakes2_predict(x),
+                                                                     cycle_length=batch_size,
+                                                                     block_length=timesteps))
+    
+    dataset = dataset.batch(timesteps).batch(batch_size)
+    dataset = dataset.prefetch(N_sub_batches * 2)
+    
+    if eager:
+        return dataset
+    
+    data_iter = dataset.make_one_shot_iterator()
+    return data_iter.get_next()
+
+
+
+def split64(d, l):
+    acousticdata = d['acousticdata'] # int16
+    tminus = l  # float64 must be split into two float32s to be compatible with input_layer
+    tminus_upper = map(lambda x: float(str(x)[:str(x).find('.') + 5]), tminus)
+    tminus_lower = (tminus - tminus_upper) * 1000000.0
+    
+    return {'acousticdata': acousticdata, 'tminus_lower': tminus_lower, 'tminus_upper': tminus_upper}
+    
+    
+def earthquake_input_fn2_64(basedir, batch_size, timesteps, scales=None, traintest='none', eager=False):
+    """
+    `scales` argument should be a list of '1eX', where X can range from -8 to 2
+    """
+    
+    assert traintest in ['train', 'test'], 'must specify traintest as "train" or "test"'
+    assert 4096 % timesteps == 0, 'timesteps must evenly divide 4096'
+    possible_scales = ['1e%d' % d for d in range(-9, 4+1)]
+    
+    files = []
+    if type(scales) is str:
+        scales = [scales]
+    for x in scales:
+        assert x in possible_scales, 'invalid scale %s, try ["1e0, 1e-2"], "1e1", etc.' % x
+        files.extend(glob(osp.join(basedir, traintest, x, '*.tfrecords')))
+    
+    dataset = tf.data.Dataset.from_tensor_slices(files)
+    if traintest == 'train':
+        dataset = dataset.shuffle(5000)
+    dataset = tf.data.TFRecordDataset(dataset, num_parallel_reads=24)
+    
+    # Turn each input example into a series of 4096/(timesteps) sub-examples
+    N_sub_batches = int(4096 / timesteps)    
+    dataset = dataset.apply(tf.data.experimental.parallel_interleave(lambda x: _deserialize_earthquakes2(x),
+                                                                     cycle_length=batch_size,
+                                                                     block_length=timesteps))
+    dataset = dataset.map(split64)
     dataset = dataset.batch(timesteps).batch(batch_size)
     dataset = dataset.prefetch(N_sub_batches * 2)
     
--- lstm_estimator.py
+++ lstm_estimator.py
@@ -8,125 +8,164 @@ def lstm_model_fn(features, labels, mode, params):
     feature_columns = params['feature_columns']
     lstm_cell_size = params['lstm_cell_size']
     learning_rate = params['learning_rate']
-    #n_fwd = params['num_fwd_lstm_cells']
-    #n_bck = params['num_bck_lstm_cells']
+    dropout_rate = params['dropout_rate']
+    
+    print('FEATURES')
+    print(features)
+    print('LABELS')
+    print(labels)
+    print('MODE')
+    print(mode)
+    print('PARAMS')
+    print(params)
     
     # Create input layer from features and feature columns
     input_layer = tf.feature_column.input_layer(features, feature_columns)
     
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        labels_input_layer = tf.feature_column.input_layer(features, params['label_input_column'], trainable=False)
+        labels_input_layer = tf.reshape(labels_input_layer, (batch_size, timesteps))
+    
     # Create LSTM layers
-    lstm_cell_fwd = tf.nn.rnn_cell.LSTMCell(num_units=lstm_cell_size,
+    lstm_cell_fwd_cells = [tf.nn.rnn_cell.LSTMCell(num_units=cell_sz,
                                             activation=tf.nn.leaky_relu,
                                             reuse=False,
                                             dtype=tf.float64,
-                                            name='lstm_cell')
+                                            name='lstm_cell') for cell_sz in lstm_cell_size]
     
-    lstm_cell_bck = tf.nn.rnn_cell.LSTMCell(num_units=lstm_cell_size,
+    lstm_cell_bck_cells = [tf.nn.rnn_cell.LSTMCell(num_units=cell_sz,
                                             activation=tf.nn.leaky_relu,
                                             reuse=False,
                                             dtype=tf.float64,
-                                            name='lstm_cell')
+                                            name='lstm_cell') for cell_sz in lstm_cell_size]
     
-    init_state = [lstm_cell_fwd.zero_state(batch_size, dtype=tf.float64),
-                  lstm_cell_bck.zero_state(batch_size, dtype=tf.float64)]
+    lstm_cell_fwd_m = tf.nn.rnn_cell.MultiRNNCell(lstm_cell_fwd_cells)
+    lstm_cell_bck_m = tf.nn.rnn_cell.MultiRNNCell(lstm_cell_bck_cells)
+    
+    init_state = [lstm_cell_fwd_m.zero_state(batch_size, dtype=tf.float64),
+                  lstm_cell_bck_m.zero_state(batch_size, dtype=tf.float64)]
+    
+    
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        lstm_cell_fwd = tf.nn.rnn_cell.DropoutWrapper(lstm_cell_fwd_m,
+                                                      input_keep_prob=dropout_rate,
+                                                      variational_recurrent=True,
+                                                      input_size=1,
+                                                      dtype=tf.float64)
+        lstm_cell_bck = tf.nn.rnn_cell.DropoutWrapper(lstm_cell_bck_m,
+                                                      input_keep_prob=dropout_rate,
+                                                      variational_recurrent=True,
+                                                      input_size=1,
+                                                      dtype=tf.float64)
     
     # Set up variable schemes
     with tf.variable_scope('lstm_state', reuse=tf.AUTO_REUSE):
-        init_state_fwd_c_zero = tf.get_variable('init_state_fwd_c_zero', 
-                                                shape=(batch_size, lstm_cell_size),
-                                                dtype=tf.float64,
-                                                trainable=False)
-        init_state_fwd_h_zero = tf.get_variable('init_state_fwd_h_zero', 
-                                                shape=(batch_size, lstm_cell_size),
-                                                dtype=tf.float64,
-                                                trainable=False)
-        init_state_bck_c_zero = tf.get_variable('init_state_bck_c_zero', 
-                                                shape=(batch_size, lstm_cell_size),
-                                                dtype=tf.float64,
-                                                trainable=False)
-        init_state_bck_h_zero = tf.get_variable('init_state_bck_h_zero', 
-                                                shape=(batch_size, lstm_cell_size),
-                                                dtype=tf.float64,
-                                                trainable=False)
+        init_state_fwd_c_zeros = [tf.get_variable('init_state_fwd_c_zero_%d' % i, 
+                                                  shape=(batch_size, cell_sz),
+                                                  dtype=tf.float64,
+                                                  trainable=False) for i, cell_sz in enumerate(lstm_cell_size)]
+        init_state_fwd_h_zeros = [tf.get_variable('init_state_fwd_h_zero_%d' % i, 
+                                                  shape=(batch_size, cell_sz),
+                                                  dtype=tf.float64,
+                                                  trainable=False) for i, cell_sz in enumerate(lstm_cell_size)]
+        init_state_bck_c_zeros = [tf.get_variable('init_state_bck_c_zero_%d' % i, 
+                                                  shape=(batch_size, cell_sz),
+                                                  dtype=tf.float64,
+                                                  trainable=False) for i, cell_sz in enumerate(lstm_cell_size)]
+        init_state_bck_h_zeros = [tf.get_variable('init_state_bck_h_zero_%d' % i, 
+                                                  shape=(batch_size, cell_sz),
+                                                  dtype=tf.float64,
+                                                  trainable=False) for i, cell_sz in enumerate(lstm_cell_size)]
+        for i in range(len(lstm_cell_size)):
+            tf.assign(init_state_fwd_c_zeros[i], init_state[0][i][0])
+            tf.assign(init_state_fwd_h_zeros[i], init_state[0][i][1])
+            tf.assign(init_state_bck_c_zeros[i], init_state[1][i][0])
+            tf.assign(init_state_bck_h_zeros[i], init_state[1][i][1])
         
-        init_state_fwd_c = tf.get_variable('init_state_fwd_c',
-                                           shape=(batch_size, lstm_cell_size),
-                                           dtype=tf.float64,
-                                           trainable=False)
-        init_state_fwd_h = tf.get_variable('init_state_fwd_h',
-                                           shape=(batch_size, lstm_cell_size),
-                                           dtype=tf.float64,
-                                           trainable=False)
-        init_state_bck_c = tf.get_variable('init_state_bck_c',
-                                           shape=(batch_size, lstm_cell_size),
-                                           dtype=tf.float64,
-                                           trainable=False)
-        init_state_bck_h = tf.get_variable('init_state_bck_h',
-                                           shape=(batch_size, lstm_cell_size),
-                                           dtype=tf.float64,
-                                           trainable=False)
+        init_state_fwd_cs = [tf.get_variable('init_state_fwd_c_%d' % i, 
+                                             shape=(batch_size, cell_sz),
+                                             dtype=tf.float64,
+                                             trainable=False) for i, cell_sz in enumerate(lstm_cell_size)]
+        init_state_fwd_hs = [tf.get_variable('init_state_fwd_h_%d' % i, 
+                                             shape=(batch_size, cell_sz),
+                                             dtype=tf.float64,
+                                             trainable=False) for i, cell_sz in enumerate(lstm_cell_size)]
+        init_state_bck_cs = [tf.get_variable('init_state_bck_c_%d' % i, 
+                                             shape=(batch_size, cell_sz),
+                                             dtype=tf.float64,
+                                             trainable=False) for i, cell_sz in enumerate(lstm_cell_size)]
+        init_state_bck_hs = [tf.get_variable('init_state_bck_h_%d' % i, 
+                                             shape=(batch_size, cell_sz),
+                                             dtype=tf.float64,
+                                             trainable=False) for i, cell_sz in enumerate(lstm_cell_size)]
         
-        tf.assign(init_state_fwd_c_zero, init_state[0][0])
-        tf.assign(init_state_fwd_h_zero, init_state[0][1])
-        tf.assign(init_state_bck_c_zero, init_state[1][0])
-        tf.assign(init_state_bck_h_zero, init_state[1][1])
-        
-        tf.assign(init_state_fwd_c, init_state_fwd_c_zero)
-        tf.assign(init_state_fwd_h, init_state_fwd_h_zero)
-        tf.assign(init_state_bck_c, init_state_bck_c_zero)
-        tf.assign(init_state_bck_h, init_state_bck_h_zero)
+        for i in range(len(lstm_cell_size)):
+            tf.assign(init_state_fwd_cs[i], init_state_fwd_c_zeros[i])
+            tf.assign(init_state_fwd_hs[i], init_state_fwd_h_zeros[i])
+            tf.assign(init_state_bck_cs[i], init_state_bck_c_zeros[i])
+            tf.assign(init_state_bck_hs[i], init_state_bck_h_zeros[i])
         
         
     
     N_sub_batches = int(4096 / timesteps)
-    assign_fwd_c = tf.assign(init_state_fwd_c, tf.cond(tf.equal(tf.floormod(tf.train.get_global_step(), N_sub_batches), 0),
-                             true_fn=lambda: init_state_fwd_c_zero,
-                             false_fn=lambda: init_state_fwd_c))
-    assign_fwd_h = tf.assign(init_state_fwd_h, tf.cond(tf.equal(tf.floormod(tf.train.get_global_step(), N_sub_batches), 0),
-                             true_fn=lambda: init_state_fwd_h_zero,
-                             false_fn=lambda: init_state_fwd_h))
-    assign_bck_c = tf.assign(init_state_bck_c, tf.cond(tf.equal(tf.floormod(tf.train.get_global_step(), N_sub_batches), 0),
-                             true_fn=lambda: init_state_bck_c_zero,
-                             false_fn=lambda: init_state_bck_c))
-    assign_bck_h = tf.assign(init_state_bck_h, tf.cond(tf.equal(tf.floormod(tf.train.get_global_step(), N_sub_batches), 0),
-                             true_fn=lambda: init_state_bck_h_zero,
-                             false_fn=lambda: init_state_bck_h))
+    
+    control_ops = []
+    for i in range(len(lstm_cell_size)):
+        assign_fwd_c = tf.assign(init_state_fwd_cs[i], tf.cond(tf.equal(tf.floormod(tf.train.get_global_step(), N_sub_batches), 0),
+                                                               true_fn=lambda: init_state_fwd_c_zeros[i],
+                                                               false_fn=lambda: init_state_fwd_cs[i]))
+        assign_fwd_h = tf.assign(init_state_fwd_hs[i], tf.cond(tf.equal(tf.floormod(tf.train.get_global_step(), N_sub_batches), 0),
+                                                               true_fn=lambda: init_state_fwd_h_zeros[i],
+                                                               false_fn=lambda: init_state_fwd_hs[i]))
+        assign_bck_c = tf.assign(init_state_bck_cs[i], tf.cond(tf.equal(tf.floormod(tf.train.get_global_step(), N_sub_batches), 0),
+                                                               true_fn=lambda: init_state_bck_c_zeros[i],
+                                                               false_fn=lambda: init_state_bck_cs[i]))
+        assign_bck_h = tf.assign(init_state_bck_hs[i], tf.cond(tf.equal(tf.floormod(tf.train.get_global_step(), N_sub_batches), 0),
+                                                               true_fn=lambda: init_state_bck_h_zeros[i],
+                                                               false_fn=lambda: init_state_bck_hs[i]))
+        
+        control_ops.extend([assign_fwd_c, assign_fwd_h, assign_bck_c, assign_bck_h])
+    
+    print(control_ops)
     
     # Prepare sub-batching for RNN state propagation
     rnn_input = tf.cast(tf.reshape(input_layer, (batch_size, timesteps, 1)), tf.float64)
     
-    with tf.control_dependencies([assign_fwd_c, assign_fwd_h, assign_bck_c, assign_bck_h]):
+    with tf.control_dependencies(control_ops):
         #rnn_inputs_by_timestep = [rnn_inputs[:,i,:n_feats] for i in range(timesteps)]
-        (outputs, states) = tf.nn.bidirectional_dynamic_rnn(cell_fw=lstm_cell_fwd,
-                                                            cell_bw=lstm_cell_bck,
-                                                            initial_state_fw=tf.nn.rnn_cell.LSTMStateTuple(init_state_fwd_c,
-                                                                                                           init_state_fwd_h),
-                                                            initial_state_bw=tf.nn.rnn_cell.LSTMStateTuple(init_state_bck_c,
-                                                                                                           init_state_bck_h),
+        (outputs, states) = tf.nn.bidirectional_dynamic_rnn(cell_fw=lstm_cell_fwd_m,
+                                                            cell_bw=lstm_cell_bck_m,
+                                                            initial_state_fw=tuple([tf.nn.rnn_cell.LSTMStateTuple(init_state_fwd_cs[i],
+                                                                                                                  init_state_fwd_hs[i])
+                                                                                    for i in range(len(lstm_cell_size))]),
+                                                            initial_state_bw=tuple([tf.nn.rnn_cell.LSTMStateTuple(init_state_bck_cs[i],
+                                                                                                                  init_state_bck_hs[i])
+                                                                                    for i in range(len(lstm_cell_size))]),
                                                             inputs=rnn_input,
                                                             dtype=tf.float64)
-        tf.assign(init_state_fwd_c, states[0][0])
-        tf.assign(init_state_fwd_h, states[0][1])
-        tf.assign(init_state_bck_c, states[1][0])
-        tf.assign(init_state_bck_h, states[1][1])
-                             
-    final_rnn_output = tf.reshape(tf.concat(outputs, axis=0), (batch_size, 2 * timesteps * lstm_cell_size))
         
-        
+        for i in range(len(lstm_cell_size)):
+            tf.assign(init_state_fwd_cs[i], states[0][i][0])
+            tf.assign(init_state_fwd_hs[i], states[0][i][1])
+            tf.assign(init_state_bck_cs[i], states[1][i][0])
+            tf.assign(init_state_bck_hs[i], states[1][i][1])
+    
+    final_rnn_output = tf.reshape(tf.concat(outputs, axis=0), (batch_size, 2 * timesteps * lstm_cell_size[-1]))
+    
     
     # Pass through a Dense layer
-    labels_shape = tf.shape(labels)
     dense = tf.layers.Dense(units=timesteps,
                             activation=None,
                             name='dense_layer',
                             dtype=tf.float64)(final_rnn_output)
     
     # Reshape dense outputs to same shape as `labels'
-    results = tf.reshape(dense, tf.shape(labels))
+    results = tf.reshape(dense, (batch_size, timesteps))
     
     if mode == tf.estimator.ModeKeys.PREDICT:
         predictions = {
-            'pred': results,
+            'labels': labels_input_layer,
+            'pred': results
         }
         return tf.estimator.EstimatorSpec(mode, predictions=predictions)
         
@@ -137,11 +176,17 @@ def lstm_model_fn(features, labels, mode, params):
     mse_op = tf.metrics.mean_squared_error(labels=labels,
                                            predictions=results,
                                            name='mse_op')
-    metrics = {'mse': mse_op}
+    mae_op = tf.metrics.mean_absolute_error(labels=labels,
+                                            predictions=results,
+                                            name='mae_op')
     tf.summary.scalar('mse', mse_op[1])
+    tf.summary.scalar('mae', mae_op[1])
+    metrics = {'mse': mse_op, 'mae': mae_op}
+    
+    
 
     if mode == tf.estimator.ModeKeys.EVAL:
-        return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=[metrics])
+        return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=metrics)
     
     
     assert mode == tf.estimator.ModeKeys.TRAIN, 'invalid mode key: ' + str(mode)
